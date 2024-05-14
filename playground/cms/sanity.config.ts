b65978d61df9682da84de2fd344dcd90c7872669
LKosters <main@laurenskosters.nl>
2024-05-14 22:43:54 +0200

    Add custom component and update sanity config

--- playground/cms/sanity.config.ts
+++ playground/cms/sanity.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'sanity'
 import { visionTool } from '@sanity/vision'
 import { debugSecrets } from '@sanity/preview-url-secret/sanity-plugin-debug-secrets'
 import { schemaTypes } from './schemas'
+import customComponent from './components/customComponent'
 
 export default defineConfig({
   name: 'default',
@@ -12,7 +13,30 @@ export default defineConfig({
   dataset: 'playground',
 
   plugins: [
-    structureTool(),
+    structureTool({
+      structure: (S, context) =>
+        S.list()
+          .title('Content')
+          .items([
+            S.listItem()
+              .title('Movies')
+              .schemaType('movie')
+              .child(
+                S.documentList()
+                  .title('Movies test')
+                  .filter('_type == "movie"')
+              ),
+            S.listItem()
+              .id('test')
+              .title('Ja Test')
+              .child(S.component(customComponent).id('example')
+                .canHandleIntent((intent, params, context) =>  {
+                    console.log(intent, params, context)
+                    return true
+                  }
+                ))
+          ])
+    }),
     visionTool(),
     presentationTool({
       previewUrl: {
